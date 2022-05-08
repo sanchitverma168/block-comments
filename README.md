47351d27918ad43b1893bfd1c488f526f6d7649c
Sanchit Verma <sanchit.verma.167@gmail.com>
2022-05-08 19:43:51 +0530

    First version release

---
+++ README.md
@@ -0,0 +1,45 @@
+# Block Comments
+
+This extension is used to create attractive commnets in your code.
+It creates a block which is highly visible for you and upcoming developer.
+It might help you and your colleague for easily understand the code.
+
+## Features
+
+Here is an screenshot attached for you reference how the editor will look after creating a new comment.
+
+![alt](images/ss1.png)
+
+## Uses Information
+
+> Press `Ctrl +alt + Home` to create a comment in the current line.
+>
+>                                  `OR`
+>
+> `Create a Block Comment here` run this command.
+>
+
+## Release Information
+
+## 0.0.1
+
+Single line comment is created for universal extension.
+
+### For more information
+
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://heconsole.log.github.com/articles/markdown-basics/)
+
+### Contact me
+
+- [Facebook](https://facebook.com/sanchitverma168)
+- [GitHub](https://github.com/sanchitverma168)
+- [Twitter](https://twitter.com/sanchitverma168)
+- [Instagram](https://www.instagram.com/sanchitverma168/)
+- [Telegram](https://t.me/s168v)
+
+[1]: http://google.com/ "Google"
+[2]: http://search.yahoo.com/ "Yahoo Search"
+[3]: http://search.msn.com/ "MSN Search"
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,119 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const fs = require('fs');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('block-comments.helloWorld', async function () {
+		// The code you place here will be executed every time your command is executed
+		let activeEditor = vscode.window.activeTextEditor;
+		// let document = activeEditor.document;
+		console.log(vscode.window.activeTextEditor);
+		let curPos = activeEditor.selection.active;
+		// console.log(curPos.line);
+		// console.log(curPos.character);
+		// console.log(curPos.translate.length);
+		// console.log(curPos.translate.name);
+		try {
+			const pos = new vscode.Position(0, curPos.character);
+			console.log(pos);
+			// let fc = fs.readFileSync(activeEditor.document.fileName);
+			// console.log(fc);
+			// const edit = new vscode.WorkspaceEdit().insert(vscode.Uri.file(activeEditor.document.fileName), pos, str);
+			const filename = activeEditor.document.fileName;
+			console.log(filename);
+			activeEditor.document.save().then(() => {
+
+				// let fc = fs.readFileSync(filename);
+				var array = fs.readFileSync(filename).toString().split("\n");
+				var dash = "// ! ||";
+				let totallength = 30;
+				for (let j = 0; j < totallength; j++) {
+					dash = dash + "-";
+				}
+				dash = dash + "||";
+
+				for (let i in array) {
+					if (parseInt(i) == curPos.line) {
+						let length = array[i].length;
+						console.log("length: tlength:" + totallength + length);
+						if (length == 0) {
+							vscode.window.showErrorMessage("Enter Some Text");
+
+						} else if (length >= totallength - 10) {
+							vscode.window.showErrorMessage("Text is too long");
+						} else {
+							let left = length % 2 == 0 ? Math.round(length / 2) : Math.round(length / 2) - 1;
+							let right = length % 2 == 0 ? length / 2 : Math.round(length / 2);
+							let temp = array[i];
+							let leftvalue = "// ! ||";
+							let rightvalue = "";
+							for (let q = 0; q < totallength / 2 - left; q++) {
+								leftvalue = leftvalue + " ";
+							}
+							for (let q = 0; q < totallength / 2 - right; q++) {
+								rightvalue = rightvalue + " ";
+							}
+							rightvalue += "||"
+							console.log("length :" + length);
+							console.log("l/2 :" + length / 2);
+							console.log("left :" + left);
+							console.log("right :" + right);
+							console.log("temp :" + temp);
+							console.log("lvalue :" + leftvalue);
+							console.log("rvalue :" + rightvalue);
+							array[i] = dash + "\n" + leftvalue + temp + rightvalue + "\n" + dash;
+							vscode.window.showInformationMessage('Comment Created Successfully');
+						}
+					}
+					// console.log(array[i]);
+				}
+				fs.writeFile(filename, array.join("\n"), function (err) {
+					if (err) {
+						console.error(err);
+						console.log("we have some error while writing file");
+					}
+					console.log("don it");
+					return;
+				});
+			});
+
+			// let succ = await vscode.workspace.applyEdit(edit);
+		} catch (error) {
+			console.error(error);
+			console.log("we are in catch block");
+		}
+		// let t = vscode.workspace.applyEdit(edit);
+		// new vscode.Selection(activeEditor.selection[],curPos.character);
+		// console.log(curPos["character"]);
+		// console.log(curPos["line"]);
+
+		// let offset = document.offsetAt(curPos);
+		// console.log(offset);
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('!!!!  TRY  NEW	!!!!! Sanchit pc', );
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
